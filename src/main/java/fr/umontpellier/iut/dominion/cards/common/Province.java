75af15a315d1eaf5802f84e80012caf96bcc936b
David Binaud <david.binaud@etu.umontpellier.fr>
2019-04-18 23:16:46 +0200

    Province getVictoryValue

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Province.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Province.java
@@ -1,6 +1,13 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.umontpellier.iut.dominion.CardType.Victory;
 
 /**
  * Carte Province
@@ -11,5 +18,13 @@ public class Province extends Card {
     public Province() {
         super("Province", 8);
     }
+
+
+    @Override
+    public int getVictoryValue(Player p) {
+        return 6;
+    }
+
+
 
 }
