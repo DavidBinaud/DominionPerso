69550bd33882cce1393ec5d4c6faf62ec02b8e7f
David Binaud <david.binaud@etu.umontpellier.fr>
2019-04-18 23:11:29 +0200

    Duchy getVictoryValue

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Duchy.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Duchy.java
@@ -1,6 +1,13 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.umontpellier.iut.dominion.CardType.Victory;
 
 /**
  * Carte Duché (Duchy)
@@ -10,6 +17,11 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Duchy extends Card {
     public Duchy() {
         super("Duchy", 5);
+    }
+
+    @Override
+    public int getVictoryValue(Player p) {
+        return 3;
     }
 
 }
