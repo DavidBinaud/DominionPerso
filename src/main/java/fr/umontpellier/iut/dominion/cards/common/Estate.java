2393d81cb384fc72e759baaff9d1342b345a64ba
David Binaud <david.binaud@etu.umontpellier.fr>
2019-04-18 23:15:21 +0200

    Estate getVictoryValue

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Estate.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Estate.java
@@ -1,6 +1,13 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.umontpellier.iut.dominion.CardType.Victory;
 
 /**
  * Carte Domaine (Estate)
@@ -10,6 +17,12 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Estate extends Card {
     public Estate() {
         super("Estate", 2);
+    }
+
+
+    @Override
+    public int getVictoryValue(Player p) {
+        return 1;
     }
 
 }
