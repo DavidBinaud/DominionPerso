93f93255f6b3d55043aae1322e442b1e3654af36
David Binaud <david.binaud@etu.umontpellier.fr>
2019-04-18 23:17:30 +0200

    Silver play

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Silver.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Silver.java
@@ -1,6 +1,13 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.umontpellier.iut.dominion.CardType.Treasure;
 
 /**
  * Carte Argent (Silver)
@@ -11,5 +18,11 @@ public class Silver extends Card {
     public Silver() {
         super("Silver", 3);
     }
+
+    @Override
+    public void play(Player p) {
+        p.incrementMoney(2);
+    }
+
 
 }
