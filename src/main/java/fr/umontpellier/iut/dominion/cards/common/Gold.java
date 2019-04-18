bf1a75f84a347fa9acc47f58ac77e027e4e55fd6
David Binaud <david.binaud@etu.umontpellier.fr>
2019-04-18 23:16:11 +0200

    Gold Play

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Gold.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Gold.java
@@ -1,7 +1,13 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.umontpellier.iut.dominion.CardType.Treasure;
 /**
  * Carte Or (Gold)
  *
@@ -11,5 +17,11 @@ public class Gold extends Card {
     public Gold() {
         super("Gold", 6);
     }
+
+    @Override
+    public void play(Player p) {
+        p.incrementMoney(3);
+    }
+
 
 }
