1231a60d7821a3fee3d6a4c805f0d488543ec10d
DavidBinaud <david.binaud@etu.umontpellier.fr>
2019-04-19 15:05:50 +0200

    Implementation de la classe Province Province hérite de Victory

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Province.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Province.java
@@ -1,5 +1,6 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
 /**
@@ -7,9 +8,13 @@ import fr.umontpellier.iut.dominion.cards.Card;
  *
  * 6 VP
  */
-public class Province extends Card {
+public class Province extends Victory {
     public Province() {
         super("Province", 8);
     }
 
+    @Override
+    public int getVictoryValue(Player p) {
+        return 6;
+    }
 }
