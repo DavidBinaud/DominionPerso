9ec15cb10db43846750f401910234ef4221b326b
DavidBinaud <david.binaud@etu.umontpellier.fr>
2019-04-19 15:06:01 +0200

    Implementation de la classe Curse Curse hérite de Card

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Curse.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Curse.java
@@ -1,6 +1,10 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.List;
 
 /**
  * Carte Malédiction (Curse)
@@ -12,4 +16,16 @@ public class Curse extends Card {
         super("Curse", 0);
     }
 
+
+    @Override
+    public List<CardType> getTypes() {
+        List<CardType> cardTypeList = super.getTypes();
+        cardTypeList.add(CardType.Curse);
+        return  cardTypeList;
+    }
+
+    @Override
+    public int getVictoryValue(Player p) {
+        return -1;
+    }
 }
