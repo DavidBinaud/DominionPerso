48ba56d8d16f6bd01b0a7083fac496247b785820
DavidBinaud <david.binaud@etu.umontpellier.fr>
2019-04-19 15:05:17 +0200

    Implementation de la classe Estate Estate hérite de Victory

--- src/main/java/fr/umontpellier/iut/dominion/cards/common/Estate.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/Estate.java
@@ -1,5 +1,6 @@
 package fr.umontpellier.iut.dominion.cards.common;
 
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
 /**
@@ -7,9 +8,13 @@ import fr.umontpellier.iut.dominion.cards.Card;
  *
  * 1 VP
  */
-public class Estate extends Card {
+public class Estate extends Victory {
     public Estate() {
         super("Estate", 2);
     }
 
+    @Override
+    public int getVictoryValue(Player p) {
+        return 1;
+    }
 }
