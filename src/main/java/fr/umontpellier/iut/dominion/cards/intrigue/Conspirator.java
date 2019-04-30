ce3246f838cfe197a641221ff8284bf1dc32a614
DavidBinaud <david.binaud@etu-umontpellier.fr>
2019-04-30 17:22:11 +0200

    Essai Bridge

--- src/main/java/fr/umontpellier/iut/dominion/cards/intrigue/Conspirator.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/intrigue/Conspirator.java
@@ -1,9 +1,31 @@
 package fr.umontpellier.iut.dominion.cards.intrigue;
 
+import fr.umontpellier.iut.dominion.CardType;
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
+import fr.umontpellier.iut.dominion.cards.Card;
 import fr.umontpellier.iut.dominion.cards.base.Action;
 
 public class Conspirator extends Action {
     public Conspirator() {
         super("Conspirator", 4);
+    }
+
+    @Override
+    public void play(Player p) {
+        p.incrementMoney(2);
+
+        ListOfCards inPlay = p.getCardsInPlay();
+        int nbActionsInPlay = 0;
+        for (Card c: inPlay) {
+            if (c.getTypes().contains(CardType.Action)){
+                nbActionsInPlay++;
+            }
+        }
+
+        if (nbActionsInPlay >= 3){
+            p.drawToHand();
+            p.incrementActions(1);
+        }
     }
 }
