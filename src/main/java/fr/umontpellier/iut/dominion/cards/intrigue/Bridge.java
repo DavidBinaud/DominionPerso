481e8afc170fa3b36b6b45581a11209bcf82c5d8
DavidBinaud <david.binaud@etu-umontpellier.fr>
2019-04-30 17:24:09 +0200

    Essai Bridge

--- src/main/java/fr/umontpellier/iut/dominion/cards/intrigue/Bridge.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/intrigue/Bridge.java
@@ -1,10 +1,18 @@
 package fr.umontpellier.iut.dominion.cards.intrigue;
 
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.base.Action;
 
 public class Bridge extends Action {
 
     public Bridge() {
         super("Bridge", 4);
+    }
+
+    @Override
+    public void play(Player p) {
+        p.incrementBuys(1);
+        p.incrementMoney(1);
+
     }
 }
