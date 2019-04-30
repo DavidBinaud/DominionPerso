f313a1150e178659964c7e6be33ecdf2f9c18540
DavidBinaud <david.binaud@etu-umontpellier.fr>
2019-04-30 17:52:59 +0200

    Courtyard

--- src/main/java/fr/umontpellier/iut/dominion/cards/intrigue/Courtyard.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/intrigue/Courtyard.java
@@ -1,9 +1,20 @@
 package fr.umontpellier.iut.dominion.cards.intrigue;
 
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.base.Action;
 
 public class Courtyard extends Action {
     public Courtyard() {
         super("Courtyard", 2);
+    }
+
+    @Override
+    public void play(Player p) {
+        p.drawToHand();
+        p.drawToHand();
+        p.drawToHand();
+
+        String input = p.chooseCard("Choose a card to put back on top of your deck",p.getCardsInHand(),false);
+        p.fromHandToDraw(p.getCardsInHand().getCard(input));
     }
 }
